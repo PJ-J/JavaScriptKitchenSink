039d496794c2d7d1bc1c33aa7a25d70b0ea2eb5e
PJ Jones <pj.jones4000.com>
2022-01-25 09:21:00 -0600

    finished this lab

--- app.js
+++ app.js
@@ -21,5 +21,28 @@ checkAge("John", 21);
 
 let favVeggies = ["onion", "lettuce", "peppers", "potatoes"];
 
+for (let i = 0; i < favVeggies.length;  i++) {
+  console.log(favVeggies[i]);
+}
+
 let pet = { name: "Henry", breed: "Bengal Mix" };
 console.log(pet);
+
+let ppl = [{ name: "Henry", age: 21 }, { name: "Jon", age: 17 }, { name: "Don", age: 33 }, { name: "Juan", age: 67 }, { name: "Glob", age: 12 }];
+
+for (let i in ppl) {
+  // console.log(checkAge(ppl[i].name, ppl[i].age));
+}
+
+let getLength = (word) => {
+  console.log(word.length);
+}
+let helloLength = getLength('Hello World');
+
+if (helloLength % 2 === 0) {
+  console.log('The world is nice and even!')
+} else {
+  console.log('The world is an odd place!');
+}
+
+
